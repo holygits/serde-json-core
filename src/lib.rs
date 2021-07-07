dedb6577d175b799d3630864b4148351f400889e
holygits <beauchjord@gmail.com>
2021-07-07 15:22:28 +1200

    fix re-export

--- src/lib.rs
+++ src/lib.rs
@@ -67,5 +67,5 @@ pub use self::de::{from_slice, from_str};
 #[doc(inline)]
 pub use self::ser::{to_string, to_vec};
 
-//! re-export heapless consts
+// re-export heapless consts
 pub use heapless::consts::{self};
